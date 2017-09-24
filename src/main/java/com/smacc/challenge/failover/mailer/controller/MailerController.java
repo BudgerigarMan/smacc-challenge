ed6a53b939ff747cfaec02fe7e7c728bf1f3b1d8
Hans Haghani <hans.haghani@sgsys.com.au>
2017-09-24 21:39:49 +1000

    Mechanism changed from SMTP to API call

--- src/main/java/com/smacc/challenge/failover/mailer/controller/MailerController.java
+++ src/main/java/com/smacc/challenge/failover/mailer/controller/MailerController.java
@@ -1,37 +1,46 @@
 package com.smacc.challenge.failover.mailer.controller;
 
-import org.apache.camel.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.sendgrid.*;
+import com.smacc.challenge.failover.mailer.model.MailRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.smacc.challenge.failover.mailer.model.MailRequest;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/mailer")
+@Slf4j
 public class MailerController {
 
-    public static final String ACTIVEMQ_EMAIL_QUEUE = "activemq:email-queue";
-
-    @Autowired
-    private CamelContext camelContext;
-
-    @Autowired
-    private ProducerTemplate producerTemplate;
+    @Value("${main.mail.api.key}")
+    String mainApiKey;
 
     @RequestMapping(value = "/send", method = RequestMethod.POST)
     public void sendEmail(@RequestBody MailRequest mailContent) throws Exception {
 
-        Endpoint endpoint = camelContext.getEndpoint(ACTIVEMQ_EMAIL_QUEUE);
-        Exchange exchange = endpoint.createExchange();
-        Message message = exchange.getIn();
-        message.getHeaders().put("From", mailContent.getSender());
-        message.getHeaders().put("To", mailContent.getReciever());
-        message.getHeaders().put("Subject", mailContent.getSubject());
-        message.setBody(mailContent.getContent());
+        Email from = new Email(mailContent.getSender());
+        String subject = mailContent.getSubject();
+        Email to = new Email(mailContent.getReceiver());
+        Content content = new Content("text/plain", mailContent.getContent());
+        Mail mail = new Mail(from, subject, to, content);
 
-        producerTemplate.send(ACTIVEMQ_EMAIL_QUEUE, exchange);
+        SendGrid sg = new SendGrid(mainApiKey);
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            log.info(response.getStatusCode() + "");
+            log.info(response.getBody());
+            log.info(response.getHeaders().toString());
+        } catch (IOException ex) {
+            throw ex;
+        }
     }
 }
 
